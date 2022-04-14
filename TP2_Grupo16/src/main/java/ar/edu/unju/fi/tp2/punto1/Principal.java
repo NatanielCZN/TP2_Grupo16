d26d2a7ae959569e8a8de8ebee075688f6af47a5
dicie <dicie@LAPTOP-TTBDTLA1>
2022-04-14 15:16:25 -0300

    punto1-commit1

--- TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto1/Principal.java
+++ TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto1/Principal.java
@@ -1,10 +1,32 @@
 package ar.edu.unju.fi.tp2.punto1;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		//COMMIT
+		int factorial = 1;
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Ingrese un numero:");
+		int numero = entrada.nextInt();
+		if (numero >= 0 && numero <= 10) {
+
+			for (int i = 1; i <= numero; i++) {
+
+				factorial = factorial * i;
+				System.out.println(factorial);
+				
+			}
+			System.out.println("EL FACTORIAL DE " + numero + " ES:" + factorial);
+		}
+
+		else {
+			System.out.println("No ingreso un numero entre el 0-10");
+
+		}
 
 	}
-
 }
+	
+
+
