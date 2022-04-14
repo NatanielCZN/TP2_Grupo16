5bb3b6484b9304b351dfa4613df46ae40bea3a20
Nataniel <natanielarg@gmail.com>
2022-04-14 15:12:04 -0300

    Clases principales de los paquetes creadas

---
+++ TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto17/Principal.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.tp2.punto17;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto3/Principal.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.tp2.punto3;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
