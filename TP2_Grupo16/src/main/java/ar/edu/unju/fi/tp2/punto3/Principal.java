c2aa6a659d70a735d4f7e66ba00d63f5dc7fc64c
dicie <dicie@LAPTOP-TTBDTLA1>
2022-04-14 15:26:49 -0300

    punto3-commit1

--- TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto3/Principal.java
+++ TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto3/Principal.java
@@ -1,10 +1,25 @@
 package ar.edu.unju.fi.tp2.punto3;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i=0;
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Ingrese 5 numeros enteros");
+		for (i = 0; i < 5; i++) {
+			System.out.print("Ingrese un numero:");
+			int numero= entrada.nextInt();
+			if (numero%2==0) {
+				System.out.println("El numero "+numero+" es Par");
+				
+			} else {
+				System.out.println("El numero "+numero+" es Impar");
+			}
+		}
+		System.out.println("cantidad de repeticiones:"+i);
+		
 	}
 
 }
