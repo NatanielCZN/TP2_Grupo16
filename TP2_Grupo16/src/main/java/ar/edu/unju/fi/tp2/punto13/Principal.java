3db17903ec7bde98cdd9470a9b38173c8adc87c6
Nataniel <natanielarg@gmail.com>
2022-04-15 18:43:25 -0300

    punto13-commit1-optimizar ingreso de dato tipo date

--- TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto13/Principal.java
+++ TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto13/Principal.java
@@ -1,10 +1,34 @@
 package ar.edu.unju.fi.tp2.punto13;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		SimpleDateFormat formateador = new SimpleDateFormat("dd/MM/yyyy");
 
+		Date fecha = new Date();
+		
+		Empleado empleado = new Empleado("Messi",fecha,10,"messirve10@gmail.com.ar",0d,60);
+		
+		int cont = 0;
+		double sum = 0;
+		do {
+			cont++;
+			if(cont < 161) {
+				sum+=600d;
+			}
+			else {
+				sum+=650d;
+			}
+		}while(cont < empleado.getHoras());
+		
+		
+		empleado.setSueldo(sum);
+		
+		String datos = empleado.toString();		
+		System.out.println(datos);
 	}
-
 }
