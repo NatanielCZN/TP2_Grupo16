56746276586cfc4954b5992ac160adc011da6a10
Nataniel <natanielarg@gmail.com>
2022-04-15 21:29:30 -0300

    punto13-commit2-problema con el ingreso de fecha resuelto

--- TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto13/Principal.java
+++ TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto13/Principal.java
@@ -1,20 +1,33 @@
 package ar.edu.unju.fi.tp2.punto13;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Principal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		Scanner scan = new Scanner(System.in);
 		
+		/*
+		 * Solicito el ingreso de dato
+		 */
+		System.out.println("Ingrese horas de trabajo: ");
+		int horas = scan.nextInt();
+		
+		/*
+		 * Cambio de formato para proporcionar un parametro mas corto
+		 */
 		SimpleDateFormat formateador = new SimpleDateFormat("dd/MM/yyyy");
-
-		Date fecha = new Date();
-		
-		Empleado empleado = new Empleado("Messi",fecha,10,"messirve10@gmail.com.ar",0d,60);
+		Empleado empleado = new Empleado("Messi",formateador.parse("16/11/2006"),10,"messirve10@gmail.com.ar",0d,horas);
 		
 		int cont = 0;
 		double sum = 0;
+		
+		/*
+		 * Realizo una suma sucesiva para obtener el sueldo
+		 */
 		do {
 			cont++;
 			if(cont < 161) {
@@ -25,9 +38,14 @@ public class Principal {
 			}
 		}while(cont < empleado.getHoras());
 		
-		
+		/*
+		 * Cambio el dato de sueldo de empleado por el resultado de la suma sucesiva
+		 */
 		empleado.setSueldo(sum);
 		
+		/*
+		 * Almaceno los datos del empleado y los muestro por consola
+		 */
 		String datos = empleado.toString();		
 		System.out.println(datos);
 	}
