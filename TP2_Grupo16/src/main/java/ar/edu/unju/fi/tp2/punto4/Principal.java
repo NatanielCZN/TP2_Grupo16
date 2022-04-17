c43dbc65ecb519737bd1a557b2f1299a9fd11975
er pueblo mehmo <er pueblo mehmo@LAPTOP-K28O79G0>
2022-04-17 15:07:59 -0300

    resolucion del punto 4 y 7

--- TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto4/Principal.java
+++ TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto4/Principal.java
@@ -1,10 +1,20 @@
 package ar.edu.unju.fi.tp2.punto4;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner entrada = new Scanner(System.in);
+		boolean band=true;
+		while (band==true) {
+			System.out.println("Ingrese numero de mes:");
+			int numero = entrada.nextInt();
+			if (numero<=0 || numero>12) {
+				System.out.println("no existe este mes");
+				band=false;
+			}
+		}
 	}
-
 }
--- TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto7/Principal.java
+++ TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto7/Principal.java
@@ -3,8 +3,10 @@ package ar.edu.unju.fi.tp2.punto7;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i=1, j;
+		for (j=40;j>0;j--) {
+			i=i+1;
+			System.out.print(j*i+" ");
+		}
 	}
-
 }
