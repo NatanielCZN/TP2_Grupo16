bfc8b208e3d9d340e3e441217528bfd01672f414
Nataniel <natanielarg@gmail.com>
2022-04-17 17:37:00 -0300

    punto14-commit1-clases Principal y Persona creadas y desarrolladas

--- TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto14/Principal.java
+++ TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto14/Principal.java
@@ -1,10 +1,17 @@
 package ar.edu.unju.fi.tp2.punto14;
 
+import java.util.Calendar;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Calendar calendario = Calendar.getInstance();
+		calendario.set(1990, 2, 28);
 
+		Persona persona = new Persona("Nataniel",calendario);
+		System.out.println("Datos de " + persona.getNombre() + ": ");
+		System.out.println("Edad: " + persona.calcularEdad());
+		System.out.println("Signo del zodiaco: " + persona.obtenerSignoDelZodiaco());
+		System.out.println("Estacion del año cuando nacio: " + persona.obtenerEstacionDelAño());
 	}
-
 }
