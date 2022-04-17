976e9c08337d0c3bd59abfa6d8d7bc7df9cfff89
Nataniel <natanielarg@gmail.com>
2022-04-17 01:07:57 -0300

    punto16-commit1-clases Principal y CalculoMatematico creadas y desarrolladas

--- TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto16/Principal.java
+++ TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto16/Principal.java
@@ -1,10 +1,18 @@
 package ar.edu.unju.fi.tp2.punto16;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Ingrese un numero para realizar calculos: ");
+		int numero = scan.nextInt();
+		
+		CalculoMatematico calculo = new CalculoMatematico(numero);
+		
+		System.out.println("El resultado de la sumatoria es: " + calculo.calcularSumatoria());
+		System.out.println("El resultado de la productoria es: " + calculo.calcularProductoria());
 	}
-
 }
