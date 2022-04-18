3d2b32b6ab285aa33ea1923d9b60a2bd44c14b72
josem <josem@LAPTOP-N7COCF30>
2022-04-17 21:41:00 -0300

    punto11-commit1-punto 11

--- TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto11/Principal.java
+++ TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto11/Principal.java
@@ -3,8 +3,12 @@ package ar.edu.unju.fi.tp2.punto11;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i=320;
+		do {
+			System.out.print(i+" ");
+			i=i-20;
+		}
+		while(i>=160);
 	}
 
 }
