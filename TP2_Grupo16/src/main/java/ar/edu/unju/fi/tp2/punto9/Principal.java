8a7a8ba034d55bbc1f349dc9ac74c00030a99af0
josem <josem@LAPTOP-N7COCF30>
2022-04-17 21:35:08 -0300

    punto9-commit1-punto 9 desarrollado

--- TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto9/Principal.java
+++ TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto9/Principal.java
@@ -3,8 +3,9 @@ package ar.edu.unju.fi.tp2.punto9;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for (int i=320 ; i>=160; i=i-20) {
+			System.out.print(i+" ");
+		}
 	}
 
 }
