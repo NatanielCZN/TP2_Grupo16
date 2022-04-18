aea10639b7ad08e60b46a501d1ec010c1b9bbe93
josem <josem@LAPTOP-N7COCF30>
2022-04-17 21:28:19 -0300

    punto2-commit1-punto2 desarrollado

--- TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto2/Principal.java
+++ TP2_Grupo16/src/main/java/ar/edu/unju/fi/tp2/punto2/Principal.java
@@ -5,11 +5,16 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
-		Scanner entrada = new Scanner(System.in);
-		System.out.println("Ingrese año: ");
-		
-		
+		int anio;
+    	Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el año");
+        anio = scanner.nextInt();
 
+        if (anio % 4 == 0 && (anio % 100 != 0 || anio % 400 == 0)){
+        	System.out.println("El año es bisiesto");
+        }
+        else {
+        	System.out.println("El año no es bisiesto");
+        }
 	}
-
 }
